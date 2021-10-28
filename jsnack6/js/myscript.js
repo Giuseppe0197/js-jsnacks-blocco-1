839f4048eed16c1309f2ede26b690876d60a2945
Giuseppe0197 <faro.g@hotmail.it>
2021-10-28 16:47:28 +0200

    sesto esercizio

---
+++ jsnack6/js/myscript.js
@@ -0,0 +1,36 @@
+/* Genera un array di 50 numeri random. Nell’array non devono esserci doppioni. */
+
+
+
+/* creiamo una variabile per stampare la lista dei numeri */
+
+const listaNumeri = document.getElementById("lista");
+
+/* creiamo un array vuota */
+
+const numList = [];
+
+console.log(numList);
+
+/* andiamo a generare i numeri */
+
+let i = 0;
+
+while (numList.length < 50){
+
+    let numRandom = Math.floor(Math.random() * 100) + 1;
+
+    if (numList.indexOf(numRandom) === -1){
+
+        numList.push(numRandom);
+
+    }
+        
+    i++
+
+}
+
+
+listaNumeri.innerHTML = numList;
+
+/* verifichiamo che i numeri non vengano ripetuti all'interno dell'array */
